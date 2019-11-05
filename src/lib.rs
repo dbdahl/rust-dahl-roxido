c71d3602661ac7d200f6702ef61801354c173eb5
David B. Dahl <dahl@stat.byu.edu>
2019-11-04 17:33:17 -0700

    Yes

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,23 @@
 
 extern crate rand;
 
+use rand::Rng;
+use rand::SeedableRng;
+use rand_isaac::IsaacRng;
+use std::slice;
+
+pub unsafe fn mk_rng<T: Rng, S>(seed_ptr: *const i32, f: S) -> T
+where T: Rng,
+      S: Fn([u8;32]) -> T
+{
+    let seed_slice = slice::from_raw_parts(seed_ptr, 32);
+    let mut seed = [0u8; 32];
+    for i in 0..seed.len() {
+        seed[i] = seed_slice[i] as u8;
+    }
+    f(seed)
+}
+
+pub unsafe fn mk_rng_isaac(seed_ptr: *const i32) -> IsaacRng {
+    mk_rng(seed_ptr, IsaacRng::from_seed)
+}
